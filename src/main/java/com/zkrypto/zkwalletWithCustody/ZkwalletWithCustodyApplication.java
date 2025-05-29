49131a6248b48715ee07e3ba98660b81d96ab359
PH5555 <lmkn5342@gmail.com>
2025-05-29 16:18:35 +0900

    first commit

---
+++ src/main/java/com/zkrypto/zkwalletWithCustody/ZkwalletWithCustodyApplication.java
@@ -0,0 +1,13 @@
+package com.zkrypto.zkwalletWithCustody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkwalletWithCustodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZkwalletWithCustodyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zkrypto/zkwalletWithCustody/ZkwalletWithCustodyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zkrypto.zkwalletWithCustody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZkwalletWithCustodyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

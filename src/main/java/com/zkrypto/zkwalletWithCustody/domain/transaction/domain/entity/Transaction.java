99672b88ef6e7defb9e3b00cec17402e835a0f91
김동현 <47862506+PH5555@users.noreply.github.com>
2025-06-02 22:06:57 +0900

    Fix: 자동 날짜 생성 오류 조치

--- src/main/java/com/zkrypto/zkwalletWithCustody/domain/transaction/domain/entity/Transaction.java
+++ src/main/java/com/zkrypto/zkwalletWithCustody/domain/transaction/domain/entity/Transaction.java
@@ -9,6 +9,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -17,6 +18,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Transaction {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;

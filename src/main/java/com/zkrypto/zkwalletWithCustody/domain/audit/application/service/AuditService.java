03976a573c63de6e7c39617bd51507dede3b2ad6
김동현 <47862506+PH5555@users.noreply.github.com>
2025-06-25 20:13:38 +0900

    Feat: AuditKey 생성 구현

---
+++ src/main/java/com/zkrypto/zkwalletWithCustody/domain/audit/application/service/AuditService.java
@@ -0,0 +1,10 @@
+package com.zkrypto.zkwalletWithCustody.domain.audit.application.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuditService {
+    public void createAuditor() {
+
+    }
+}

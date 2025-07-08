32791ac97b523292348caa2e4827224eb3b4a963
김동현 <47862506+PH5555@users.noreply.github.com>
2025-07-08 10:36:24 +0900

    Test: 트랜잭션 레포지토리 테스트

--- src/test/java/com/zkrypto/zkwalletWithCustody/transaction/TransactionRepositoryTest.java
+++ src/test/java/com/zkrypto/zkwalletWithCustody/transaction/TransactionRepositoryTest.java
@@ -1,27 +1,38 @@
 package com.zkrypto.zkwalletWithCustody.transaction;
 
+import com.zkrypto.zkwalletWithCustody.domain.corporation.application.dto.request.CorporationCreationCommand;
+import com.zkrypto.zkwalletWithCustody.domain.corporation.application.service.CorporationService;
+import com.zkrypto.zkwalletWithCustody.domain.corporation.domain.entity.Corporation;
 import com.zkrypto.zkwalletWithCustody.domain.transaction.domain.entity.Transaction;
 import com.zkrypto.zkwalletWithCustody.domain.transaction.domain.repository.TransactionRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigInteger;
 import java.util.Optional;
 
+@Slf4j
 @SpringBootTest
 public class TransactionRepositoryTest {
 
     @Autowired
     private TransactionRepository transactionRepository;
+    @Autowired
+    private CorporationService corporationService;
 
+    @Transactional
     @BeforeEach
     void 트랜잭션_생성() {
+        Corporation corporation = corporationService.createCorporation(new CorporationCreationCommand("지크립토토"));
+
         Transaction transaction1 = new Transaction();
         transaction1.setBlockNumber(BigInteger.ZERO);
-        transactionRepository.save(transaction1);
+        transaction1.setSender(corporation);
 
         Transaction transaction2 = new Transaction();
         transaction2.setBlockNumber(BigInteger.ONE);
@@ -32,5 +43,13 @@ public class TransactionRepositoryTest {
     void 블럭_조회() {
         Optional<BigInteger> maxBlockNumber = transactionRepository.findMaxBlockNumber();
         Assertions.assertThat(maxBlockNumber.get().toString()).isEqualTo("1");
+    }
+
+    @Transactional
+    @Test
+    void 트랜잭션_조회() {
+        Optional<Transaction> transactionByIdWithCorporation = transactionRepository.findTransactionByIdWithCorporation(1L);
+        BigInteger blockNumber = transactionByIdWithCorporation.get().getBlockNumber();
+        Assertions.assertThat(transactionByIdWithCorporation.get().getSender().getName()).isEqualTo("지크립토토");
     }
 }
